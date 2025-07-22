ff94ed52754eb5a2c735f6da7e4630e903e98483
Dhanush <dhanushpgowda73@gmail.com>
2025-07-22 11:51:42 +0530

    Refactor App component: improve state handling and structure for better readability

--- src/App.js
+++ src/App.js
@@ -6,44 +6,50 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
-function App() {
+export default function App() {
   const [currentStep, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (currentStep > 1) setStep(currentStep - 1);
+    if (currentStep > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (currentStep < 3) setStep(currentStep + 1);
+    if (currentStep < 3) setStep((s) => s + 1);
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={currentStep >= 1 ? "active" : ""}>1</div>
-        <div className={currentStep >= 2 ? "active" : ""}>2</div>
-        <div className={currentStep >= 3 ? "active" : ""}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={currentStep >= 1 ? "active" : ""}>1</div>
+            <div className={currentStep >= 2 ? "active" : ""}>2</div>
+            <div className={currentStep >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {currentStep}: {messages[currentStep - 1]}
-      </p>
+          <p className="message">
+            Step {currentStep}: {messages[currentStep - 1]}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "white" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "white" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
-export default App;
